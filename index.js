b580205b255d1ff5930e5bbe2f336c643057aec1
Jason Veng <jasonveng@outlook.com>
2023-07-23 22:36:01 +1200

    small tweaks

--- index.js
+++ index.js
@@ -12,32 +12,34 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-$(window).scroll(function() {
-  
-  // selectors
-  let $window = $(window),
+$(window)
+  .scroll(function () {
+    // selectors
+    let $window = $(window),
       $body = $('body'),
       $hidden = $('.hidden');
-  
-  // Change 33% earlier than scroll position so colour is there when you arrive.
-  let scroll = $window.scrollTop() + ($window.height() / 3);
- 
-  $hidden.each(function () {
-    let $this = $(this);
-    
-    // if position is within range of this panel.
-    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-    // Remember we set the scroll to 33% earlier in scroll var.
-    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-          
-      // Remove all classes on body with color-
-      $body.removeClass(function (index, css) {
-        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-      });
-       
-      // Add class of currently active div
-      $body.addClass('color-' + $(this).data('color'));
-    }
-  });    
-  
-}).scroll();
+
+    // Change 33% earlier than scroll position so colour is there when you arrive.
+    let scroll = $window.scrollTop() + $window.height() / 3;
+
+    $hidden.each(function () {
+      let $this = $(this);
+
+      // if position is within range of this panel.
+      // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+      // Remember we set the scroll to 33% earlier in scroll var.
+      if (
+        $this.position().top <= scroll &&
+        $this.position().top + $this.height() > scroll
+      ) {
+        // Remove all classes on body with color-
+        $body.removeClass(function (index, css) {
+          return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
+        });
+
+        // Add class of currently active div
+        $body.addClass('color-' + $(this).data('color'));
+      }
+    });
+  })
+  .scroll();
